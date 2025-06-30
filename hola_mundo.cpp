dd459ea7efbc45d3746e0876e9e0501384e3d4f9
TizianaW <tizianasandovalt2@gmail.com>
2025-06-30 20:52:37 -0300

    Add files via upload

---
+++ hola_mundo.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Librería que sirve para mostrar el texto
+using namespace std;
+
+int main() {
+    cout << "¡Hola, mundo! 🌎" << endl;  // muestra el texto que elijas (en este caso hola mundo)
+    return 0;
+}
